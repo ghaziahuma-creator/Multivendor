75534e1e1b2fcd0505b715a2e64b93ef06fea947
Ghazia Huma <ghaziahuma@gmail.com>
2026-09-08 15:40:45 +0500

    Using enviromental variables for frontend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000/"
+// export const server = "http://localhost:8000/api/v2";
+// export const backend_url = "http://localhost:8000/"
+export const server = `${import.meta.env.VITE_BACKEND_URL}/api/v2`;
+export const backend_url = `${import.meta.env.VITE_BACKEND_URL}/`;
