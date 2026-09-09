844f633dca94c2ad843240ee753e423f8fcce97a
Ghazia Huma <ghaziahuma@gmail.com>
2026-09-09 16:20:55 +0500

    Updated image appearence in chat

--- frontend/src/pages/UserInbox.jsx
+++ frontend/src/pages/UserInbox.jsx
@@ -369,7 +369,7 @@ const UserChatBox = ({
               )}
               {item.images && (
                 <img
-                  src={`${item.images.url}`}
+                  src={`${item?.images?.url}`}
                   className="w-[300px] h-[300px] object-cover rounded-[10px] mr-2"
                 />
               )}
